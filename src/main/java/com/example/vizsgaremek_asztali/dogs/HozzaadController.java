18791a8794587641bca55db18d3dc76ccb023cf2
Kovács Krisztián <k.krisztian0830@gmail.com>
2022-02-22 12:20:16 +0100

    hozzáadás ablak kinézete

--- src/main/java/com/example/vizsgaremek_asztali/dogs/HozzaadController.java
+++ src/main/java/com/example/vizsgaremek_asztali/dogs/HozzaadController.java
@@ -1,7 +1,30 @@
 package com.example.vizsgaremek_asztali.dogs;
 
 import com.example.vizsgaremek_asztali.Controller;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 
 public class HozzaadController extends Controller {
 
+    @FXML
+    private TextField nevInput;
+    @FXML
+    private DatePicker szulidoInput;
+    @FXML
+    private Spinner KedvelésInput;
+    @FXML
+    private TextField fajInput;
+    @FXML
+    private TextArea leirasInput;
+    @FXML
+    private TextArea kultulInput;
+    @FXML
+    private ComboBox nemInput;
+    @FXML
+    private ComboBox orokbefogadasInput;
+
+    @FXML
+    public void onHozzaadas(ActionEvent actionEvent) {
+    }
 }
