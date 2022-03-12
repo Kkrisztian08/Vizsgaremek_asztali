2013f030411121a4e3b0f5018cf183f4ac6a9ee2
Kkrisztian08 <k.krisztian0830@gmail.com>
2022-03-12 20:53:17 +0100

    Adoptions és AdoptionApi osztályok kész

--- src/main/java/com/example/vizsgaremek_asztali/adoption/AdoptionApi.java
+++ src/main/java/com/example/vizsgaremek_asztali/adoption/AdoptionApi.java
@@ -14,5 +14,24 @@ public class AdoptionApi {
     private static final String API_URL = BASE_URL + "/api/adoption";
     private static Gson jsonConverted = new Gson();
 
+    public static List<Adoptions> get() throws IOException {
+        String json = Api.get(API_URL);
+        Type type = new TypeToken<List<Cats>>(){}.getType();
+        return jsonConverted.fromJson(json,type);
+    }
+    public static Adoptions post(Adoptions uj) throws IOException {
+        String ujJson = jsonConverted.toJson(uj);
+        String json = Api.post(API_URL, ujJson);
+        return jsonConverted.fromJson(json, Adoptions.class);
+    }
 
+    public static boolean delete(int id) throws IOException {
+        return Api.delete(API_URL, id).getResponseCode() == 240;
+    }
+
+    public static Adoptions put(Adoptions modosit) throws IOException {
+        String modositandoJson = jsonConverted.toJson(modosit);
+        String json = Api.put(API_URL,modosit.getId(), modositandoJson);
+        return jsonConverted.fromJson(json, Adoptions.class);
+    }
 }
--- src/main/java/com/example/vizsgaremek_asztali/adoption/Adoptions.java
+++ src/main/java/com/example/vizsgaremek_asztali/adoption/Adoptions.java
@@ -1,4 +1,33 @@
 package com.example.vizsgaremek_asztali.adoption;
 
 public class Adoptions {
+    private int id;
+    private int adoptionTypeId;
+    private String begin;
+
+    public Adoptions(int id, int adoptionTypeId, String begin) {
+        this.id = id;
+        this.adoptionTypeId = adoptionTypeId;
+        this.begin = begin;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAdoptionTypeId() {
+        return adoptionTypeId;
+    }
+
+    public void setAdoptionTypeId(int adoptionTypeId) {
+        this.adoptionTypeId = adoptionTypeId;
+    }
+
+    public String getBegin() {
+        return begin;
+    }
+
+    public void setBegin(String begin) {
+        this.begin = begin;
+    }
 }
