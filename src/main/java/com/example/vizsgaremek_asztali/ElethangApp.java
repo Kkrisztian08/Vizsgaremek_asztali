3b8ef24586c22d6b971cbeb164093e6b08d374e8
Kkrisztian08 <k.krisztian0830@gmail.com>
2022-03-16 12:16:18 +0100

    bejelentkezési felület kész, oldal váltás müködik

--- src/main/java/com/example/vizsgaremek_asztali/ElethangApp.java
+++ src/main/java/com/example/vizsgaremek_asztali/ElethangApp.java
@@ -11,8 +11,9 @@ import java.io.IOException;
 public class ElethangApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(ElethangApp.class.getResource("FXML/dogs/dogs-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
+        FXMLLoader fxmlLoader = new FXMLLoader(ElethangApp.class.getResource("FXML/login-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 400, 400);
+        //Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
         stage.setTitle("Élethang alapítvány");
         stage.getIcons().add(new Image(ElethangApp.class.getResourceAsStream("icons/logo_szerkesztve.png")));
         stage.setScene(scene);
--- src/main/java/com/example/vizsgaremek_asztali/LoginController.java
+++ src/main/java/com/example/vizsgaremek_asztali/LoginController.java
@@ -1,4 +1,58 @@
 package com.example.vizsgaremek_asztali;
 
-public class LoginController {
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Control;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController extends Controller {
+    @FXML
+    private TextField felhasznaloInput;
+    @FXML
+    private PasswordField jelszoInput;
+
+    @FXML
+    public void onBejelentkezes(ActionEvent actionEvent) {
+        String felhasznalo=felhasznaloInput.getText().trim();
+        String jelszo=jelszoInput.getText().trim();
+
+        boolean hiba =false;
+        StringBuilder alertBuilder=new StringBuilder();
+
+
+        if (felhasznalo.isEmpty()){
+            //alert("A név megadása kötelező");
+            felhasznaloInput.getStyleClass().add("error");
+            alertBuilder.append("A felhasználonév megadása kötelező").append(System.lineSeparator());
+            hiba=true;
+        }
+        if (jelszo.isEmpty()){
+            //alert("A név megadása kötelező");
+            jelszoInput.getStyleClass().add("error");
+            alertBuilder.append("A jelszó megadása kötelező").append(System.lineSeparator());
+            hiba=true;
+        }
+
+        if (hiba) {
+            alert(alertBuilder.toString());
+            return;
+        }
+
+        try {
+            Controller oldalvaltas = ujAblak("FXML/dogs/dogs-view.fxml", "Kutyák Tábla",
+                    1100, 600);
+            oldalvaltas.getStage().show();
+            this.stage.close();
+        } catch (Exception e) {
+            hibaKiir(e);
+        }
+    }
+
+    @FXML
+    public void hibaVege(Event event) {
+        Control control = (Control) event.getSource();
+        control.getStyleClass().remove("error");
+    }
 }
